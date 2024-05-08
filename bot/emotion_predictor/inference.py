0ebf5e3f5cd4040718677db07df5c995e1341dbf
yangyx30678 <yangyx30678@gmail.com>
2024-05-09 00:18:16 +0800

    add inference with py file

---
+++ bot/emotion_predictor/inference.py
@@ -0,0 +1,77 @@
+import os
+import pandas as pd
+import torch
+from datasets import load_dataset, Dataset
+import pandas as pd
+from sklearn.metrics import f1_score, accuracy_score
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+from huggingface_hub import login
+
+# login(token=os.environ.get("HF_TOKEN", ""), add_to_git_credential=True)
+
+new_model = "checkpoints"
+base_model = "michellejieli/emotion_text_classifier"
+tokenizer = AutoTokenizer.from_pretrained(base_model)
+
+
+def preprocessing(data):
+    data = data.rename_column("utterance", "text")
+    data = data.rename_column("emotion", "label")
+    data = data.remove_columns(["dialog_id", "turn_type"])
+    return data
+
+def shifting_test(data):
+    df = data.to_pandas()
+    df["label"] = df["label"].shift(-1).fillna(0).astype(int)
+    modified_dataset = Dataset.from_pandas(df)
+    data = modified_dataset
+    return data
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+num_labels = 7
+id2label = {
+    0: "neutral",
+    1: "anger",
+    2: "disgust",
+    3: "fear",
+    4: "happiness",
+    5: "sadness",
+    6: "surprise"
+}
+
+label2id = {
+    "neutral": 0,
+    "anger": 1,
+    "disgust": 2,
+    "fear": 3,
+    "happiness": 4,
+    "sadness": 5,
+    "surprise": 6
+}
+
+
+classifier_model = AutoModelForSequenceClassification.from_pretrained(new_model, num_labels=num_labels, id2label=id2label, label2id=label2id)
+
+classifier = pipeline("sentiment-analysis", model=classifier_model, tokenizer=tokenizer, device=0)
+data_name = "benjaminbeilharz/better_daily_dialog"
+data = load_dataset(data_name, split='test', num_proc=8)
+data = preprocessing(data)
+data = shifting_test(data)
+
+def predict(row):
+    text = row['text']
+    true_label = row['label']
+    predicted_result = classifier(text)[0]
+    predicted_label = label2id[predicted_result["label"]]
+
+    return {"predicted_label": predicted_label, "true_label": true_label}
+predictions = data.map(predict)
+true_labels = [p["true_label"] for p in predictions]
+predicted_labels = [p["predicted_label"] for p in predictions]
+
+f1 = f1_score(true_labels, predicted_labels, average='weighted')
+accuracy = accuracy_score(true_labels, predicted_labels)
+
+print("F1-score:", f1, )
+print("Accuracy:", accuracy)
