2f530c958d4a38a9b4dbef4bbf24e683872b138d
yangyx30678 <yangyx30678@gmail.com>
2024-05-24 17:38:17 +0800

    fix: wrong f1 and acc on original classifier

--- src/bot/sentiment_analysis/inference.py
+++ src/bot/sentiment_analysis/inference.py
@@ -48,34 +48,52 @@ label2id = {
     "surprise": 6
 }
 
+og_label2id = {
+    "neutral": 0,
+    "anger": 1,
+    "disgust": 2,
+    "fear": 3,
+    "joy": 4,
+    "sadness": 5,
+    "surprise": 6
+}
+
 data_name = "benjaminbeilharz/better_daily_dialog"
 data = load_dataset(data_name, split='test', num_proc=8)
 data = preprocessing(data)
-
+print(data[0:5])
 classifier_model = AutoModelForSequenceClassification.from_pretrained(new_model, num_labels=num_labels, id2label=id2label, label2id=label2id)
 classifier = pipeline("sentiment-analysis", model=classifier_model, tokenizer=tokenizer, device=0)
 
 predictions = data.map(predict)
 true_labels = [p["true_label"] for p in predictions]
 predicted_labels = [p["predicted_label"] for p in predictions]
+ft_10 = predicted_labels[0:9]
 
 f1_ft = f1_score(true_labels, predicted_labels, average='weighted')
 accuracy_ft = accuracy_score(true_labels, predicted_labels)
 
-classifier_model = AutoModelForSequenceClassification.from_pretrained(base_model, num_labels=num_labels, id2label=id2label, label2id=label2id)
-classifier = pipeline("sentiment-analysis", model=classifier_model, tokenizer=tokenizer, device=0)
 
-predictions = data.map(predict)
+classifier_model = AutoModelForSequenceClassification.from_pretrained(base_model)
+classifier = pipeline("sentiment-analysis", model=classifier_model, tokenizer=tokenizer, device=0)
+def predict_origin(row):
+    text = row['text']
+    true_label = row['label']
+    predicted_result = classifier(text)[0]
+    predicted_label = og_label2id[predicted_result["label"]]
+    return {"predicted_label": predicted_label, "true_label": true_label}
+
+predictions = data.map(predict_origin)
 true_labels = [p["true_label"] for p in predictions]
 predicted_labels = [p["predicted_label"] for p in predictions]
-
+og_10 = predicted_labels[0:9]
 f1 = f1_score(true_labels, predicted_labels, average='weighted')
 accuracy = accuracy_score(true_labels, predicted_labels)
 
 print("Fine-tuned:")
 print("F1-score:", f1_ft, )
 print("Accuracy:", accuracy_ft)
-
 print("\nOriginal:")
 print("F1-score:", f1, )
 print("Accuracy:", accuracy)
+print("true:", true_labels[0:10], "\nfine-tuned:", ft_10, "\noriginal:", og_10)
